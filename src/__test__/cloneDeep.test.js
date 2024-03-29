e8387fd79e4526c478fb639e6c99b9c768ac9817
zeromountain <sonyeongsan@gmail.com>
2024-03-29 14:17:57 +0900

    apply feedback

--- src/__test__/cloneDeep.test.js
+++ src/__test__/cloneDeep.test.js
@@ -144,9 +144,7 @@ describe("cloneDeep 함수 Set 테스트", () => {
     const copy = cloneDeep(originSet);
     originSet.add(4);
 
-    const results = Array.from(originSet.entries()).map(([key, _]) =>
-      copy.has(key)
-    );
+    const results = Array.from(originSet).map(([key, _]) => copy.has(key));
 
     console.log({ results });
 
